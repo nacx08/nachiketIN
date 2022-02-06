73bd9a2423ce94c26debb9599a732e9fa803850b
nachiket-at-677807702011 <nachiket@medputdirect.com>
2022-02-06 23:03:18 +0530

    commit

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 "use strict";
-import { success, failure } from "Library";
+import { success, failure } from "Lib";
 
 module.exports.hello = async (event) => {
   var Result = { message: "Message not found!!!" };
