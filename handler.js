b659d09ae229c4e1fc6b1ecbc75ccd5adb146702
nachiket-at-677807702011 <nachiket@medputdirect.com>
2022-02-08 11:57:06 +0530

    commit

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 import { success, failure } from "./Library/response";
+import obj from "/data.json";
 
 exports.initialize = async (event) => {
+  console.log(obj);
   var Result = { message: "Message not found!!!" };
   return success(Result);
 };
