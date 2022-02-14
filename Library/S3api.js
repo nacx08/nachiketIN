f1e8c4419ff2d641ca3316c19431d12dd78f2a5f
nachiket-at-677807702011 <nachiket@medputdirect.com>
2022-02-14 22:56:07 +0530

    commit

---
+++ Library/S3api.js
@@ -0,0 +1,307 @@
+import { SentryTrack } from "Sentry";
+import * as AWS from "aws-sdk";
+const AWS_BUCKET = process.env.AWS_BUCKET;
+const AWS_BUCKET_JSON = process.env.AWS_BUCKET_JSON;
+const AWS_KEY = process.env.AWS_KEY;
+const AWS_SECRET = process.env.AWS_SECRET;
+
+class S3api {
+  #_s3;
+  #_bucketName;
+  #_bucketNameJson;
+  #_folderName;
+  #_fileName;
+
+  set s3(value) {
+    this.#_s3 = value;
+  }
+  get s3() {
+    return this.#_s3;
+  }
+
+  set bucketName(value) {
+    this.#_bucketName = value;
+  }
+  get bucketName() {
+    return this.#_bucketName;
+  }
+
+  set folderName(value) {
+    this.#_folderName = value;
+  }
+  get folderName() {
+    return this.#_folderName;
+  }
+
+  set fileName(value) {
+    this.#_fileName = value;
+  }
+  get fileName() {
+    return this.#_fileName;
+  }
+
+  constructor() {
+    this.#_s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      accessKeyId: AWS_KEY,
+      secretAccessKey: AWS_SECRET,
+    });
+    AWS.config.update({ region: "us-east-1" });
+    this.#_bucketName = AWS_BUCKET;
+    this.#_bucketNameJson = AWS_BUCKET_JSON;
+  }
+
+  deleteObject = async () => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = { Bucket: this.#_bucketName, Key: this.#_fileName };
+        var ReturnObjectIn = this.#_s3
+          .deleteObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        SentryTrack.trackError(
+          "error while deleting object in s3 bucket",
+          error
+        );
+        reject({
+          error: "Sequelize error while deleting object in s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  objectExists = async () => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = { Bucket: this.#_bucketName, Key: this.#_fileName };
+        var ReturnObjectIn = this.#_s3
+          .waitFor("objectExists", params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        SentryTrack.trackError(
+          "error while searching for object in s3 bucket",
+          error
+        );
+        reject({
+          error:
+            "Sequelize error while searching for object in s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  listObjects = async () => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = { Bucket: this.#_bucketName };
+        if (this.folderName) {
+          params.Delimiter = "/";
+          params.Prefix = this.folderName;
+        }
+        var ReturnObjectIn = this.#_s3
+          .listObjects(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        SentryTrack.trackError("error while fetching list s3 bucket", error);
+        reject({
+          error: "Sequelize error while fetching list s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  getData = async () => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = {
+          Bucket: this.#_bucketName,
+          Key: this.#_fileName,
+        };
+        var ReturnObjectIn = this.#_s3
+          .getObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        reject({
+          error: "Sequelize error while upload Data to s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  uploadData = async (bodyContent, contentType) => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = {
+          // ACL: 'public-read',
+          Bucket: this.#_bucketName,
+          Key: this.#_fileName,
+          Body: bodyContent,
+          ContentType: contentType,
+        };
+        var ReturnObjectIn = this.#_s3
+          .putObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        SentryTrack.trackError("error while upload Data to s3 bucket", error);
+        reject({
+          error: "Sequelize error while upload Data to s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  uploadDataJson = async (bodyContent, contentType, userEvent) => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        const RootfolderPath = `useroperations/${userEvent}/${Date.now()}.json`;
+        var params = {
+          // ACL: 'public-read',
+          Bucket: this.#_bucketNameJson,
+          Key: RootfolderPath,
+          Body: JSON.stringify(bodyContent),
+          ContentType: contentType,
+        };
+        var ReturnObjectIn = await this.#_s3
+          .putObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        console.log(error);
+        SentryTrack.trackError("error while upload Data to s3 bucket", error);
+        reject({
+          error: "Sequelize error while upload Data to s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  uploadStream = async (bodyContent) => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        var params = {
+          // ACL: 'public-read',
+          Bucket: this.#_bucketName,
+          Key: this.#_fileName,
+          Body: bodyContent,
+        };
+        var ReturnObjectIn = this.#_s3
+          .putObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        SentryTrack.trackError("error while upload Data to s3 bucket", error);
+        reject({
+          error: "Sequelize error while upload Data to s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+
+  uploadDataJsonWithBucketName = async (
+    bodyContent,
+    contentType,
+    userEvent,
+    bucketName
+  ) => {
+    var ReturnObject = new Promise(async (resolve, reject) => {
+      try {
+        const RootfolderPath = `useroperations/${userEvent}/${Date.now()}.json`;
+        var params = {
+          // ACL: 'public-read',
+          Bucket: bucketName,
+          Key: RootfolderPath,
+          Body: JSON.stringify(bodyContent),
+          ContentType: contentType,
+        };
+        var ReturnObjectIn = await this.#_s3
+          .putObject(params)
+          .promise()
+          .then(
+            (data) => {
+              return data;
+            },
+            (err) => {
+              return err;
+            }
+          );
+        resolve(ReturnObjectIn);
+      } catch (error) {
+        console.log(error);
+        SentryTrack.trackError("error while upload Data to s3 bucket", error);
+        reject({
+          error: "Sequelize error while upload Data to s3 bucket: " + error,
+        });
+      }
+    });
+    return ReturnObject;
+  };
+}
+export default S3api;
--- handler.js
+++ handler.js
@@ -3,90 +3,96 @@ var XLSX = require("xlsx");
 var path = require("path");
 var moment = require("moment");
 const fs = require("fs");
-const axios = require("axios").default;
+const Downloader = require("nodejs-file-downloader");
 
 exports.initialize = async (event) => {
   // Url of the image
+  var result = {};
   var fileName = "";
   var dt = new Date();
   var n = dt.getDay();
   if (n == 0) {
     dt = addDays(dt, -1);
+  } else {
+    dt = addDays(dt, 1);
   }
 
   dt = dt.toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
   console.log(dt);
+  var filePathFull = path.join(__dirname, "files/", fileName);
+  fileName = await downloadUrl(dt, filePathFull);
 
-  fileName = await downloadUrl(dt);
+  filePathFull = path.join(filePathFull, fileName);
+  if (fs.existsSync(filePathFull)) {
+    var workbook = XLSX.readFile(filePathFull);
+    console.log("fileName::", fileName);
+    var sheet_name_list = workbook.SheetNames;
+    var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    for (var i = 0; i < xlData.length; i++) {
+      xlData[i]["DailyPercent"] =
+        xlData[i][
+          "Current Day Underlying Daily Volatility (E) = Sqrt(0.995*D*D + 0.005*C*C)"
+        ] * 100;
+      xlData[i]["AnnualPercent"] =
+        xlData[i]["Underlying Annualised Volatility (F) = E*Sqrt(365)"] * 100;
+    }
 
-  var filePathFull = __dirname + "/files/" + fileName;
-  var workbook = XLSX.readFile(filePathFull);
-  var sheet_name_list = workbook.SheetNames;
-  var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-  for (var i = 0; i < xlData.length; i++) {
-    xlData[i]["DailyPercent"] =
-      xlData[i][
-        "Current Day Underlying Daily Volatility (E) = Sqrt(0.995*D*D + 0.005*C*C)"
-      ] * 100;
-    xlData[i]["AnnualPercent"] =
-      xlData[i]["Underlying Annualised Volatility (F) = E*Sqrt(365)"] * 100;
+    var Nifty50 = [];
+    Nifty50.push("ONGC");
+    Nifty50.push("TATASTEEL");
+    Nifty50.push("INFY");
+    Nifty50.push("SBILIFE");
+    Nifty50.push("HDFCBANK");
+    Nifty50.push("GRASIM");
+    Nifty50.push("KOTAKBANK");
+    Nifty50.push("M&M");
+    Nifty50.push("HDFC");
+    Nifty50.push("JSWSTEEL");
+    Nifty50.push("POWERGRID");
+    Nifty50.push("TATAMOTORS");
+    Nifty50.push("TECHM");
+    Nifty50.push("SBIN");
+    Nifty50.push("HINDALCO");
+    Nifty50.push("WIPRO");
+    Nifty50.push("NTPC");
+    Nifty50.push("BAJAJFINSV");
+    Nifty50.push("HCLTECH");
+    Nifty50.push("TATACONSUM");
+    Nifty50.push("HDFCLIFE");
+    Nifty50.push("SUNPHARMA");
+    Nifty50.push("CIPLA");
+    Nifty50.push("AXISBANK");
+    Nifty50.push("LT");
+    Nifty50.push("INDUSINDBK");
+    Nifty50.push("EICHERMOT");
+    Nifty50.push("BRITANNIA");
+    Nifty50.push("BHARTIARTL");
+    Nifty50.push("BAJFINANCE");
+    Nifty50.push("ICICIBANK");
+    Nifty50.push("ASIANPAINT");
+    Nifty50.push("HINDUNILVR");
+    Nifty50.push("BAJAJ - AUTO");
+    Nifty50.push("TITAN");
+    Nifty50.push("HEROMOTOCO");
+    Nifty50.push("DIVISLAB");
+    Nifty50.push("NESTLEIND");
+    Nifty50.push("DRREDDY");
+    Nifty50.push("BPCL");
+    Nifty50.push("UPL");
+    Nifty50.push("RELIANCE");
+    Nifty50.push("COALINDIA");
+    Nifty50.push("ADANIPORTS");
+    Nifty50.push("ULTRACEMCO");
+    Nifty50.push("SHREECEM");
+    Nifty50.push("IOC");
+    Nifty50.push("MARUTI");
+
+    result = xlData.filter(function (x) {
+      return x["DailyPercent"] > 3 && Nifty50.indexOf(x.Symbol) > -1;
+    });
+
+    console.log(result);
   }
-
-  var Nifty50 = [];
-  Nifty50.push("ONGC");
-  Nifty50.push("TATASTEEL");
-  Nifty50.push("INFY");
-  Nifty50.push("SBILIFE");
-  Nifty50.push("HDFCBANK");
-  Nifty50.push("GRASIM");
-  Nifty50.push("KOTAKBANK");
-  Nifty50.push("M&M");
-  Nifty50.push("HDFC");
-  Nifty50.push("JSWSTEEL");
-  Nifty50.push("POWERGRID");
-  Nifty50.push("TATAMOTORS");
-  Nifty50.push("TECHM");
-  Nifty50.push("SBIN");
-  Nifty50.push("HINDALCO");
-  Nifty50.push("WIPRO");
-  Nifty50.push("NTPC");
-  Nifty50.push("BAJAJFINSV");
-  Nifty50.push("HCLTECH");
-  Nifty50.push("TATACONSUM");
-  Nifty50.push("HDFCLIFE");
-  Nifty50.push("SUNPHARMA");
-  Nifty50.push("CIPLA");
-  Nifty50.push("AXISBANK");
-  Nifty50.push("LT");
-  Nifty50.push("INDUSINDBK");
-  Nifty50.push("EICHERMOT");
-  Nifty50.push("BRITANNIA");
-  Nifty50.push("BHARTIARTL");
-  Nifty50.push("BAJFINANCE");
-  Nifty50.push("ICICIBANK");
-  Nifty50.push("ASIANPAINT");
-  Nifty50.push("HINDUNILVR");
-  Nifty50.push("BAJAJ - AUTO");
-  Nifty50.push("TITAN");
-  Nifty50.push("HEROMOTOCO");
-  Nifty50.push("DIVISLAB");
-  Nifty50.push("NESTLEIND");
-  Nifty50.push("DRREDDY");
-  Nifty50.push("BPCL");
-  Nifty50.push("UPL");
-  Nifty50.push("RELIANCE");
-  Nifty50.push("COALINDIA");
-  Nifty50.push("ADANIPORTS");
-  Nifty50.push("ULTRACEMCO");
-  Nifty50.push("SHREECEM");
-  Nifty50.push("IOC");
-  Nifty50.push("MARUTI");
-
-  var result = xlData.filter(function (x) {
-    return x["DailyPercent"] > 3 && Nifty50.indexOf(x.Symbol) > -1;
-  });
-
-  console.log(result);
   return success(result);
 };
 
@@ -101,42 +107,41 @@ function addDays(date, days) {
   return result;
 }
 
-const downloadUrl = async function (dt) {
+const downloadUrl = async function (dt, filePathFull) {
   dt = addDays(dt, -1);
   var fileName = `CMVOLT_${moment(dt).format("DDMMYYYY")}.CSV`;
   //const fileName = "CMVOLT_10022022.csv";
 
+  if (fs.existsSync(path.join(filePathFull, fileName))) {
+    fs.unlinkSync(path.join(filePathFull, fileName));
+  }
+  console.log("filePathFull::", path.join(filePathFull, fileName));
   var downLoadURL = `https://www1.nseindia.com/archives/nsccl/volt/${fileName}`;
   // Path at which image will get downloaded
-
+  console.log(downLoadURL);
   try {
-    var filePath = __dirname + "/files/";
+    var filePath = path.join(__dirname, "files/");
+    console.log(filePath);
     await download(downLoadURL, filePath);
     return fileName;
   } catch (ex) {
-    fileName = await downloadUrl(dt);
+    //fileName = await downloadUrl(dt,filePathFull);
     return fileName;
   }
 };
 
 const download = async (fileUrl, downloadFolder) => {
-  // Get the file name
-  const fileName = path.basename(fileUrl);
-
-  // The path of the downloaded file on our machine
-  const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
+  const downloader = new Downloader({
+    url: fileUrl, //If the file name already exists, a new file with the name 200MB1.zip is created.
+    directory: downloadFolder, //This folder will be created, if it doesn't exist.
+  });
   try {
-    const response = await axios({
-      method: "GET",
-      url: fileUrl,
-      responseType: "stream",
-    });
+    await downloader.download(); //Downloader.download() returns a promise.
 
-    const w = response.data.pipe(fs.createWriteStream(localFilePath));
-    w.on("finish", () => {
-      console.log("Successfully downloaded file!");
-    });
-  } catch (err) {
-    throw new Error(err);
+    console.log("All done");
+  } catch (error) {
+    //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
+    //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
+    console.log("Download failed", error);
   }
 };
